332fcd17796e754d36393c80d460a56836d067a8
GodsplanELot <elotgodsplan2@gmail.com>
2025-01-02 11:28:53 +0100

    installed and testing dependencies

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('testing⚡');
+const { format } = require('date-fns');
+const {v4: uuid } = require('uuid');
+
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
+
+console.log(uuid());
+
+
