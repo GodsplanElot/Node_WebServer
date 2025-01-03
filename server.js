2e3366763d24b3b18a0954907a6f5e38c15289de
GodsplanELot <elotgodsplan2@gmail.com>
2025-01-03 10:27:27 +0100

    Writing the server logic for the webserver

--- server.js
+++ server.js
@@ -1,16 +1,65 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
 const logEvent = require('./logEvent');
-
 const EventEmitter = require('events');
-
-class MyEmitter extends EventEmitter {};
-
+class Emitter extends EventEmitter {};
 //initialze object
-const myEmitter = new MyEmitter();
+const myEmitter = new Emitter();
 
-//add listener for the log event
-myEmitter.on('log', (msg) => logEvent(msg));
+const PORT = process.env.PORT || 3500;
 
-setTimeout(() =>{
-    //Emmit Event
-    myEmitter.emit('log', 'log event emitted!');
-}, 2000);
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+            
+    }
+
+    let filePath = 
+        contentType === 'text/html' && req.url ==='/'
+        ? path.join(__dirname, 'views', 'index.html')
+        :contentType === 'text/html' && req.url.slice(-1) === '/'
+            ? path.join(__dirname, 'views', req.url, 'index.html')
+            : contentType === 'text/html'
+                ? path.join(__dirname, 'views', req.url)
+                : path.join(__dirname, req.url);
+
+    // makes the .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+});
+
+server.listen(PORT, () => console.log (`server running on port ${PORT}`));
+
+
+/* myEmitter.on('log', (msg) => logEvent(msg));
+
+    myEmitter.emit('log', 'log event emitted!'); */
